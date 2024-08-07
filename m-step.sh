d98382837278294450d721074d60b78a232c6989
wmb <bwm320924@163.com>
2024-08-07 13:33:34 +0800

    Add files via upload

--- m-step.sh
+++ m-step.sh
@@ -1,15 +1,3 @@
-#! /bin/bash
-#SBATCH -o log/output.%j.out
-#SBATCH --partition=GPUA100
-#SBATCH --job-name=ciden
-#SBATCH --ntasks=1
-#SBATCH --gres=gpu:6
-#SBATCH --qos=normal
-#SBATCH --cpus-per-task=5
-#SBATCH --time 100:00:00
-#SBATCH --mem 24G
-#SBATCH -x gpua800n23,gpua800n02,gpua800n15
-
 source /gpfs/share/software/anaconda/3-2023.09-0/etc/profile.d/conda.sh
 conda activate DPS
 
